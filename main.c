ab44810a59c0db2a1737ea95dddc7a01bd202dfa
Stephen Magill <stephen@muse.dev>
2023-06-30 19:30:43 -0400

    Initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int x, y, z;
+
+    printf("Enter an integer: ");
+    scanf("%d", &x);
+
+    printf("Enter an integer: ");
+    scanf("%d", &y);
+
+    z = x * y;
+
+    printf("x * y = %d\n", z);
+
+    return 0;
+}
